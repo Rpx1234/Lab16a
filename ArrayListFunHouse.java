b1da0e774446f506aca564d1eedc7fb932e0eef9
Rpx1234 <22777764+Rpx1234@users.noreply.github.com>
2019-01-31 11:21:48 -0600

    Add files via upload

---
+++ ArrayListFunHouse.java
@@ -0,0 +1,31 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Lab  - 
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import static java.lang.System.*;
+
+public class ArrayListFunHouse
+{
+	/*
+	 *method getListOfFactors will return a list of 
+	 *all of the factors of number - excluding number
+	 */
+	public static ArrayList<Integer> getListOfFactors(int number)
+	{
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for(int i = 1; i < number; i++)
+		{
+			if(number % i ==0)
+			{
+				list.add(i);
+			}
+		}
+			//add code here
+		return list;
+		
+	}
+}
